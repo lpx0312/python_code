bc0fbc37a60a573166ea85ebbea514fac74b0817
wy <=lpx_0312@163.com>
2022-04-24 13:29:47 +0800

    futrue模块未完待续

---
+++ 00_常用模块/37_兼容性模块/future模块、/使用future模块.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：python_code 
+@File    ：使用future模块.py
+@Author  ：lipanxiang
+@Date    ：2022/4/24 8:04 
+'''
+
+# 参考文档:
+# https://blog.csdn.net/langb2014/article/details/53305246
+# https://blog.csdn.net/weixin_39450145/article/details/103873062
+# https://www.cnblogs.com/iamls/p/12874991.html
+# https://www.jianshu.com/p/14b156c361c4
+# https://ccie.lol/knowledge-base/python-char/
+# https://cloud.tencent.com/developer/article/1349021
+
+# 兼容python2和python3的 print
+from __future__ import print_function
+print(1,2)  # 兼容后  仅兼容py3的写法
+#
--- 00_常用模块/37_兼容性模块/six模块/01_使用six后.py
+++ 00_常用模块/37_兼容性模块/six模块/01_使用six后.py
@@ -7,15 +7,10 @@
 @Date    ：2022/4/22 8:33 
 '''
 
-def import_utf8():
-    import six
-    print("six")
 
-import six
-# import_utf8()
-print(six.PY2)
 # 解决python2中打印返回中文，python3返回b'\xe5\x91\xb5\xe5\x91\xb5\xe5\x91\xb5\xe5\x91\xb5\n'的问题
 def convert_to_unicode(text):
+    import six
     """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
     if six.PY3:
         if isinstance(text, str):
@@ -34,14 +29,39 @@ def convert_to_unicode(text):
     else:
         raise ValueError("Not running on Python2 or Python 3?")
 
+import urllib.request
+response = urllib.request.urlopen('http://www.baidu.com')
+html = response.read()
+print(html)
+
+import os
+ret=os.popen('bash ~/20.sh')
+print(ret.read())
 
 
-# import urllib.request
-# response = urllib.request.urlopen('http://www.baidu.com')
-# html = convert_to_unicode(response.read())
-# print(html)
-#
-# import urllib.request
-# response = urllib.request.urlopen('http://www.baidu.com')
-# html = response.read()
-# print(html)
+
+import urllib.request
+response = urllib.request.urlopen('http://www.baidu.com')
+html = convert_to_unicode(response.read())
+print(html)
+
+
+
+
+import urllib.request
+response = urllib.request.urlopen('http://www.baidu.com')
+html = response.read()
+print(html)
+
+
+import urllib.request
+from __future__ import unicode_literals
+response = urllib.request.urlopen('http://www.baidu.com')
+html = response.read()
+print(html)
+
+
+
+
+
+
