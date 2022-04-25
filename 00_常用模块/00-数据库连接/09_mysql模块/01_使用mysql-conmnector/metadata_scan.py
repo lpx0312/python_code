2a198561f8be56fe5de73136ca73ffe8d0666cc2
wy <=lpx_0312@163.com>
2022-04-25 14:29:49 +0800

    源数据扫描完成

--- 00_常用模块/00-数据库连接/09_mysql模块/01_使用mysql-conmnector/metadata_scan.py
+++ 00_常用模块/00-数据库连接/09_mysql模块/01_使用mysql-conmnector/metadata_scan.py
@@ -24,35 +24,43 @@ config={
 }
 
 def ignore_dir(rootdir,path):
-    ignore_lst=['/.git/','/.idea/']
+    ignore_lst=['/.git','/.idea','/.gitignore']
     for ignore in ignore_lst:
         if ignore in os.path.join(path).replace(rootdir,''):
             return True
     return False
 
 def insert_mysql(total_file_path,projectName):
+    # print('call insert_mysql')
     mydb = mysql.connector.connect(**config)
     mycursor = mydb.cursor()
     sql='insert into dev_icehck (project_name,path) VALUES (%s,%s)'
     mycursor.execute(sql,(projectName,total_file_path))
     mydb.commit()
-    print("{0}条数据插入成功".format(mycursor.rowcount))
+    #print("{0}条数据插入成功".format(mycursor.rowcount))
 
 def metadata_scan(rootdir,projectName):
+    print('scan_code_rootdir:::{0}'.format(rootdir))
     paths=os.walk(rootdir,topdown=False)
+    mfiles=[]
     for path, dir_lst , file_lst in paths:
         # 如果包含有隐藏文件的忽略掉。
+        # print("path::::",path)
         if ignore_dir(rootdir,path):
+            # print("path:::: 忽略", path)
             continue
         # 注意windows和linux斜线的不一样
-        if "\metedata\\" not in os.path.join(path):
+        if "/metedata/" not in os.path.join(path):
             continue
         # 如果不包含,metedata目录 也跳过。
         for file in file_lst:
             file_path=os.path.join(path,file)
             total_file_path = file_path[len(rootdir):]
-            print(total_file_path,projectName)
+            # print(total_file_path,projectName)
             insert_mysql(total_file_path,projectName)
+            mfiles.append((total_file_path,projectName))
+    print('scan_metadata_num:::{0}'.format(len(mfiles)))
+
 
 def getargs():
     parser = argparse.ArgumentParser('源数据扫描', prefix_chars='-')
@@ -61,12 +69,12 @@ def getargs():
     args = parser.parse_args()
     return vars(args)
 
-
 if __name__ == '__main__' :
-    # python metadata_scan.py  --rootdir="/mnt/disk" --projectName="hmos
+    # python metadata_scan.py  --rootdir="/root/python_code" --projectName="hmos"
     args = getargs()
     rootdir = args.get('rootdir')
     projectName = args.get('projectName')
+    print(args)
     if not rootdir or not projectName :
         print('metadata_scan.py args is failed')
         sys.exit(1)
